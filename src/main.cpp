399d54c7beddcb2c185b4c4d2cbba2ffa5374dbe
turtlology <504350677@qq.com>
2018-06-20 00:11:08 -0400

    establish system structure

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//#include <tbb/tbb.h>
+//#include <opencv/cv.h>
+//#include <rapidjson/rapidjson.h>
+#include <boost/any.hpp>
+//#include <CUDA/cuda.h>
+
+
+using namespace std;
+
+
+int main() {
+//    std::cout << __VERSION__ << std::endl;
+    return 0;
+}
