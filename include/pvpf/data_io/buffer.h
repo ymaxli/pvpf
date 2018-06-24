e9fe4182e8cf996f09a0fe9501657b959e5e70e7
MaxLi1994 <liyue4158@gmail.com>
2018-06-23 21:48:10 -0400

    add new data_io.buffer and its test suite

--- include/pvpf/data_io/buffer.h
+++ include/pvpf/data_io/buffer.h
@@ -7,16 +7,87 @@
 #pragma once
 
 #include "pvpf/pvpf.h"
+#include <mutex>
+#include <thread>
+#include <chrono>
+#include "pvpf/utils/data_bucket.h"
 
 PVPF_NAMESPACE_BEGIN
 
-    namespace data_io
-    {
-        class Buffer {
-        private: int a;
+    namespace data_io {
+        class buffer {
         public:
-            void write(int a);
-            int read();
+            buffer(int buffer_size, bool blocking) : buffer_size(buffer_size), blocking(blocking) {
+                write_index = 0;
+                read_index = 0;
+                buffer_array = new data_bucket[buffer_size];
+                buffer_array_read_available = new bool[buffer_size];
+                for (int i = 0; i < buffer_size; i++) buffer_array_read_available[i] = false;
+            }
+
+            ~buffer() {
+                delete [] buffer_array;
+                delete [] buffer_array_read_available;
+            }
+
+            void write(data_bucket data) {
+                if (blocking) {
+                    while (buffer_array_read_available[write_index]) {
+                        std::this_thread::sleep_for(std::chrono::milliseconds(BLOCK_INTERVAL));
+                    }
+                }
+
+                if (write_index == read_index) {
+                    mutex.try_lock();
+                    write_to_current_position(data);
+                    mutex.unlock();
+                } else {
+                    write_to_current_position(data);
+                }
+
+            }
+
+            data_bucket read() {
+                while (!buffer_array_read_available[read_index]) {
+                    std::this_thread::sleep_for(std::chrono::milliseconds(BLOCK_INTERVAL));
+                }
+
+                data_bucket result;
+
+                if (write_index == read_index) {
+                    mutex.try_lock();
+                    result = read_from_current_position();
+                    mutex.unlock();
+                } else {
+                    result = read_from_current_position();
+                }
+
+                return result;
+            }
+
+        private:
+            const int BLOCK_INTERVAL = 100;
+            int buffer_size;
+            bool blocking;
+            int write_index, read_index;
+            std::mutex mutex;
+            data_bucket *buffer_array; // use a dynamically-allocated array to contain buffer
+            bool *buffer_array_read_available; // flag for buffer read availability
+            void write_to_current_position(data_bucket data) {
+                buffer_array[write_index] = data;
+                buffer_array_read_available[write_index] = true;
+                write_index = (write_index + 1) % buffer_size;
+            }
+
+            data_bucket read_from_current_position() {
+                data_bucket result;
+
+                buffer_array_read_available[read_index] = false;
+                result = buffer_array[read_index];
+                read_index = (read_index + 1) % buffer_size;
+
+                return result;
+            }
         };
     }
 
--- unittest/data_io.buffer.cpp
+++ unittest/data_io.buffer.cpp
@@ -2,22 +2,99 @@
 // Created by Max on 2018/6/23.
 //
 
-#ifdef STAND_ALONE
-#define BOOST_TEST_MODULE DemoTests
-#endif
+//#ifdef STAND_ALONE
+//#define BOOST_TEST_MODULE DemoTests
+//#endif
 
 #include <boost/test/unit_test.hpp>
 #include <pvpf/data_io/buffer.h>
+#include <pvpf/utils/data_bucket.h>
+#include <boost/any.hpp>
+#include <thread>
+#include <chrono>
+#include <iostream>
+
+#define THREAD_WAITING_TIME 500
 
 using namespace pvpf::data_io;
+using namespace std;
 
 BOOST_AUTO_TEST_SUITE(data_io_buffer_suite)
 
-    BOOST_AUTO_TEST_CASE(sample_test)
-    {
-        Buffer b;
-        b.write(3);
-        BOOST_CHECK_EQUAL(3, b.read());
+    BOOST_AUTO_TEST_CASE(write_blocking) {
+        buffer b(5, true);
+        data_bucket bucket;
+        int k = 111;
+        bucket.put("abc", k);
+
+        thread first([&]() -> void {
+            b.write(bucket);
+            b.write(bucket);
+            b.write(bucket);
+            b.write(bucket);
+            b.write(bucket);
+            b.write(bucket);
+            cout << "then write" << endl;
+        });
+        thread second([&]() -> void {
+            this_thread::sleep_for(chrono::milliseconds(THREAD_WAITING_TIME));
+            cout << "read first" << endl;
+            b.read();
+        });
+
+        first.join();
+        second.join();
+    }
+
+    BOOST_AUTO_TEST_CASE(write_non_blocking) {
+        buffer b(5, false);
+        data_bucket bucket;
+        int k = 111;
+        bucket.put("abc", k);
+
+        thread first([&]() -> void {
+            b.write(bucket);
+            b.write(bucket);
+            b.write(bucket);
+            b.write(bucket);
+            b.write(bucket);
+            b.write(bucket);
+            b.write(bucket);
+            b.write(bucket);
+            b.write(bucket);
+            cout << "write finished first" << endl;
+        });
+        thread second([&]() -> void {
+            this_thread::sleep_for(chrono::milliseconds(THREAD_WAITING_TIME));
+            b.read();
+            this_thread::sleep_for(chrono::milliseconds(THREAD_WAITING_TIME));
+            b.read();
+            cout << "then read finished" << endl;
+        });
+
+        first.join();
+        second.join();
+    }
+
+    BOOST_AUTO_TEST_CASE(read_waiting) {
+        buffer b(5, false);
+        data_bucket bucket;
+        int k = 111;
+        bucket.put("abc", k);
+
+        thread first([&]() -> void {
+            this_thread::sleep_for(chrono::milliseconds(THREAD_WAITING_TIME));
+            cout << "write first" << endl;
+            b.write(bucket);
+
+        });
+        thread second([&]() -> void {
+            b.read();
+            cout << "then read" << endl;
+        });
+
+        first.join();
+        second.join();
     }
 
 BOOST_AUTO_TEST_SUITE_END()
