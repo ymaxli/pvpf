7f91538498d85fe282b34d938b2d0a3c177b2feb
zszyellow <2581113549@qq.com>
2018-06-25 15:55:32 -0400

    add rules

--- src/config/config_validator.cpp
+++ src/config/config_validator.cpp
@@ -13,18 +13,19 @@ PVPF_NAMESPACE_BEGIN
 
 namespace config
 {
-    vector<validation_result const> config_validator::validate(Document &conf) {
+
+    vector<validation_result const> config_validator::validate(Document &conf)
+    {
         vector<validation_result const> res;
 
         // check json format
-        if(!conf.IsObject()) {
-            const validation_result vr(2, "invalid JSON file");
-            res.push_back(vr);
+        concrete_rule_format rule_format = concrete_rule_format();
+        const validation_result format = rule_format.validate(conf);
+        res.push_back(format);
+        //if invalid format, return immediately
+        if (format.type == 2)
+        {
             return res;
-        }
-        else {
-            const validation_result vr(0, "valid JSON file");
-            res.push_back(vr);
         }
 
         //check source field
@@ -32,14 +33,20 @@ namespace config
         const validation_result source = rule_source.validate(conf);
         res.push_back(source);
 
+        //check graph field
+        concrete_rule_graph rule_graph = concrete_rule_graph();
+        const validation_result graph = rule_graph.validate(conf);
+        res.push_back(graph);
 
+        //check sink field
+        concrete_rule_sink rule_sink = concrete_rule_sink();
+        const validation_result sink = rule_sink.validate(conf);
+        res.push_back(sink);
 
 
         return res;
 
-
     }
-
 }
 
 PVPF_NAMESPACE_END
--- src/config/validation_rule.cpp
+++ src/config/validation_rule.cpp
@@ -11,13 +11,138 @@ PVPF_NAMESPACE_BEGIN
 
 namespace config
 {
-    validation_result concrete_rule_source::validate(Document &conf) {
-        if(!conf.HasMember("source")){
-            return validation_result(2, "lack “source” field");
+    validation_result concrete_rule_format::validate(rapidjson::Document &conf) {
+        // rule 1: check general json format
+        if(!conf.IsObject()) {
+            return validation_result(2, "invalid JSON file");
         }
-        //TODO
+        else {
+            return validation_result(0, "valid JSON file");
+        }
     }
+
+    validation_result concrete_rule_source::validate(Document &conf) {
+        // rule 2: check source field
+        if(!conf.HasMember("source")){
+            return validation_result(2, "Error: lack “source” field");
+        }
+        // rule 3: check source field is empty or not
+        else if(conf["source"].Empty()){
+            return validation_result(2, "Error: “source” field has to have at least one child");
+        }
+        else if (!conf["source"].IsArray()) {
+            return validation_result(2, "Error: the content of “source” field is not an array");
+        }
+        else if (conf["source"].IsArray()) {
+            const Value& source = conf["source"];
+            for (SizeType i = 0; i < source.Size; i ++) {
+                // rule 8: check node id
+                if (!source[i].HasMember("id")) {
+                    return validation_result(2, "Error: node has to have an id");
+                }
+                // rule 10: check task field of source node
+                if (!source[i].HasMember("task")) {
+                    return validation_result(2, "Error: source node has to have a task field");
+                }
+                // rule 12: check output field of source node
+                if (!source[i].HasMember("output")) {
+                    return validation_result(2, "Error:  source node has to have an output field");
+                }
+            }
+
+        }
+        else {
+            return validation_result(0, "Pass: source field check");
+        }
+    }
+
+    validation_result concrete_rule_graph::validate(Document &conf) {
+        // rule 4: check graph field
+        if(!conf.HasMember("graph")){
+            return validation_result(2, "Error: lack “graph” field");
+        }
+        // rule 5: check graph field is empty or not
+        else if(conf["graph"].Empty()){
+            return validation_result(2, "Error: “graph” field has to have at least one child");
+        }
+        else if (!conf["graph"].IsArray()) {
+            return validation_result(2, "Error: the content of “graph” field is not an array");
+        }
+        else if (conf["graph"].IsArray())
+        {
+            const Value &graph = conf["graph"];
+            for (SizeType i = 0; i < graph.Size(); i ++)
+            {
+                // rule 8: check node id
+                if (! graph[i].HasMember("id"))
+                {
+                    return validation_result(2, "Error: node has to have an id");
+                }
+                // rule 14: check input field of graph node
+                if (! graph[i].HasMember("input"))
+                {
+                    return validation_result(2, "Error: graph node has to have an input field");
+                }
+                // rule 15: : check task field of graph node
+                if (! graph[i].HasMember("task"))
+                {
+                    return validation_result(2, "Error: graph node has to have an task field");
+                }
+                    // rule 16: check algorithm of graph node
+                else
+                {
+                    if (graph[i]["task"].HasMember("algorithm"))
+                    {
+                        return validation_result(2, "Error: algorithm not found");
+                    }
+                }
+            }
+        }
+        else {
+            return validation_result(0, "Pass: graph field check");
+        }
+    }
+
+    validation_result concrete_rule_sink::validate(Document &conf) {
+        // rule 6: check sink field
+        if(!conf.HasMember("sink")){
+            return validation_result(2, "Error: lack “sink” field");
+        }
+        // rule 7: check sink field is empty or not
+        else if(conf["sink"].Empty())
+        {
+            return validation_result(2, "Error: “sink” field has to have at least one child");
+        }
+        else if (!conf["sink"].isArray()) {
+            return validation_result(2, "Error: the content of “sink” field is not an array");
+        }
+        else if (conf["sink"].isArray) {
+            const Value& sink = conf["sink"];
+            for (SizeType i = 0; i < sink.Size(); i ++) {
+                // rule 8: check node id
+                if (!sink[i].HasMember("id")) {
+                    return validation_result(2, "Error: node has to have an id");
+                }
+                // rule 11: check task field of sink node
+                if (!sink[i].HasMember("task")) {
+                    return validation_result(2, "Error: A sink node missing the task field");
+                }
+                // rule 13: check input field of sink node
+                if (!sink[i].HasMember("input")) {
+                    return validation_result(2, "Error: sink node has to have an input field");
+                }
+            }
+        }
+        else {
+            return validation_result(0, "Pass: sink field check");
+        }
+    }
+
+    //TODO
+    // rule 9: check duplicate node
 }
+
+
 
 PVPF_NAMESPACE_END
 
