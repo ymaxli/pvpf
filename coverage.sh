5b9fb41d7c74cc013a59674854838b2bb9ee536c
turtlology <504350677@qq.com>
2018-07-02 15:53:04 -0400

    add auto open

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd build/unit_test/CMakeFiles/pvpf_unit_tests.dir
 lcov --capture --directory . --output-file test.info --test-name test
-genhtml test.info --output-directory output --title "a simple test" --show-details --legend
+genhtml test.info --output-directory output --title "coverage" --show-details --legend
 cp -r output ../../../coverage
+cd ../../../coverage
+open index.html
