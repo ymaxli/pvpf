9ffef9254e98f1820877c77e16da036721319c33
MaxLi1994 <liyue4158@gmail.com>
2018-07-02 15:17:49 -0400

    improve testing script

--- system_test.sh
+++ system_test.sh
@@ -2,8 +2,8 @@
 cmake build
 cd build
 find . -name "*.gcda" | xargs rm -rf
-make
 cd system_test
+make
 if [ $1 ]; then
     ./point_vision_pipeline_framework_system_tests $1
 else
--- unit_test.sh
+++ unit_test.sh
@@ -2,8 +2,8 @@
 cmake build
 cd build
 find . -name "*.gcda" | xargs rm -rf
-make
 cd unit_test
+make
 if [ $1 ]; then
     ./point_vision_pipeline_framework_unit_tests $1
 else
