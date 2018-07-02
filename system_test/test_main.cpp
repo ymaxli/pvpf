73d936629e3ee0e86d4e5d5d8b14eaaf5b4f0271
turtlology <504350677@qq.com>
2018-07-02 15:10:32 -0400

    add a shell script to run the system test

--- system_test/test_main.cpp
+++ system_test/test_main.cpp
@@ -1,2 +1,2 @@
-#define BOOST_TEST_MODULE DemoTests
+#define BOOST_TEST_MODULE SYSTEMTEST
 #include <boost/test/unit_test.hpp>
--- unit_test/test_main.cpp
+++ unit_test/test_main.cpp
@@ -1,2 +1,2 @@
-#define BOOST_TEST_MODULE DemoTests
+#define BOOST_TEST_MODULE UNITTEST
 #include <boost/test/unit_test.hpp>
