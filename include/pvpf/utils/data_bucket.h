6c805cec750d9421fe1583e09f10dcd33fdb724b
MaxLi1994 <liyue4158@gmail.com>
2018-07-01 22:03:03 -0400

    make data_bucket much better

--- include/pvpf/utils/data_bucket.h
+++ include/pvpf/utils/data_bucket.h
@@ -4,43 +4,80 @@
 
 #ifndef DEV_ENV_DATA_BUCKET_H
 #define DEV_ENV_DATA_BUCKET_H
+#pragma once
 
-#include <boost/any.hpp>
+#include <core/any.hpp>
 #include <map>
+#include "pvpf/pvpf.h"
 
-class data_bucket {
-private:
-    std::map<std::string, boost::any> map;
-public:
-    template<typename T>
-    void put(std::string const & key, T & value) {
-        map[key] = value;
-    }
+PVPF_NAMESPACE_BEGIN
 
-    boost::any & get(std::string const & key) {
-        return map[key];
-    }
-
-    data_bucket(const data_bucket &db) {
-        for( std::map<std::string, boost::any>::const_iterator it = db.map.begin(); it != db.map.end(); ++it )
-        {
-            std::string key = it->first;
-            boost::any value = it->second;
-            map[key] = value;
+    class data_bucket {
+    private:
+        std::map<std::string, std::shared_ptr<core::any>> *map;
+    public:
+        data_bucket() {
+            map = new std::map<std::string, std::shared_ptr<core::any>>();
         }
-    }
 
-    data_bucket() {
-    }
+        ~data_bucket() {
+            delete map;
+        }
 
-    std::map<std::string, boost::any>::iterator begin() {
-        return this->map.begin();
-    }
+        data_bucket(const data_bucket &new_data_bucket) = delete;
 
-    std::map<std::string, boost::any>::iterator end() {
-        return this->map.end();
-    }
-};
+        data_bucket(data_bucket &&new_data_bucket) : map(new_data_bucket.map) {
+            new_data_bucket.map = nullptr;
+        }
 
+        data_bucket &operator=(data_bucket &&new_data_bucket) {
+            if (this != &new_data_bucket) {
+                map = new_data_bucket.map;
+                new_data_bucket.map = nullptr;
+            }
+
+            return *this;
+        }
+
+        template<typename T>
+        void put(std::string const &key, T &&value) {
+            (*map)[key] = std::make_shared<core::any>(std::forward<T>(value));
+        }
+
+        template<typename T>
+        T get_copy(std::string const &key) {
+            if (!(map->count(key))) throw std::string("key:") + key + std::string(" does not exist");
+
+            std::weak_ptr<core::any> ptr = (*map)[key];
+            if (auto spt = ptr.lock()) {
+                return core::any_cast<T>(*spt);
+            } else {
+                throw std::string("key:") + key + std::string(" does not exist");
+            }
+        }
+
+        template<typename T>
+        T *get_ptr(std::string const &key) {
+            if (!(map->count(key))) throw std::string("key:") + key + std::string(" does not exist");
+
+            std::weak_ptr<core::any> ptr = (*map)[key];
+            if (auto spt = ptr.lock()) {
+                T *ptr = core::any_cast<T>(spt.get());
+                return ptr;
+            } else {
+                throw std::string("key:") + key + std::string(" does not exist");
+            }
+        }
+
+        std::map<std::string, std::shared_ptr<core::any>>::iterator begin() {
+            return this->map->begin();
+        }
+
+        std::map<std::string, std::shared_ptr<core::any>>::iterator end() {
+            return this->map->end();
+        }
+    };
+
+PVPF_NAMESPACE_END
 
 #endif //DEV_ENV_DATA_BUCKET_H
---
+++ unit_test/utils.data_bucket.cpp
@@ -0,0 +1,206 @@
+//
+// Created by Max on 2018/7/1.
+//
+#ifdef STAND_ALONE
+#define BOOST_TEST_MODULE DemoTestinginginging
+#endif
+
+#include <boost/test/unit_test.hpp>
+#include <pvpf/utils/data_bucket.h>
+#include <iostream>
+#include <core/any.hpp>
+
+using namespace std;
+using namespace pvpf;
+
+int constructor_count = 0;
+int destructor_count = 0;
+int copy_count = 0;
+int move_count = 0;
+
+class A {
+public:
+    bool output;
+    string id;
+    int content;
+
+    A(bool out, string &&id = "a", int content = 0) :
+            output(out), id(id), content(content) {
+        constructor_count++;
+        print("constructor");
+    }
+
+    ~A() {
+        destructor_count++;
+        print("destructor");
+    }
+
+    A(const A &a) : output(a.output), id(a.id + ".copy"), content(a.content) {
+        copy_count++;
+        print("copy constructor");
+    }
+
+    A(A &&a) : output(a.output), id(a.id + ".move"), content(a.content) {
+        move_count++;
+        print("move constructor");
+    }
+
+private:
+    void print(string &&str) {
+        if (output) {
+            cout << id + " " + str << endl;
+        }
+    }
+};
+
+void reset_counters() {
+    constructor_count = 0;
+    destructor_count = 0;
+    copy_count = 0;
+    move_count = 0;
+}
+
+BOOST_AUTO_TEST_SUITE(utils_data_bucket_suite)
+
+    BOOST_AUTO_TEST_CASE(copy_put) {
+        reset_counters();
+
+        {
+            A a(false);
+            data_bucket bucket;
+
+            bucket.put("abc", a);
+        }
+
+        BOOST_TEST(constructor_count + copy_count + move_count == destructor_count);
+        BOOST_TEST(constructor_count == 1);
+        BOOST_TEST(move_count == 0);
+        BOOST_TEST(copy_count == 1);
+    }
+
+    BOOST_AUTO_TEST_CASE(move_put) {
+        reset_counters();
+
+        {
+            A a(false);
+            data_bucket bucket;
+
+            bucket.put("abc", std::move(a));
+        }
+
+        BOOST_TEST(constructor_count + copy_count + move_count == destructor_count);
+        BOOST_TEST(constructor_count == 1);
+        BOOST_TEST(move_count == 1);
+        BOOST_TEST(copy_count == 0);
+    }
+
+    BOOST_AUTO_TEST_CASE(copy_override_an_entry) {
+        reset_counters();
+
+        {
+            A a(false, "a");
+            A b(false, "b");
+            data_bucket bucket;
+
+            bucket.put("abc", a);
+            bucket.put("abc", b);
+        }
+
+        BOOST_TEST(constructor_count + copy_count + move_count == destructor_count);
+        BOOST_TEST(constructor_count == 2);
+        BOOST_TEST(move_count == 0);
+        BOOST_TEST(copy_count == 2);
+    }
+
+    BOOST_AUTO_TEST_CASE(move_override_an_entry) {
+        reset_counters();
+
+        {
+            A *a = new A(false, "a");
+            A b(false, "b");
+            data_bucket bucket;
+
+            bucket.put("abc", std::move(*a));
+            bucket.put("abc", std::move(b));
+            delete a;
+        }
+
+        BOOST_TEST(constructor_count + copy_count + move_count == destructor_count);
+        BOOST_TEST(constructor_count == 2);
+        BOOST_TEST(move_count == 2);
+        BOOST_TEST(copy_count == 0);
+    }
+
+    BOOST_AUTO_TEST_CASE(move_maintain_life_time) {
+        reset_counters();
+
+        {
+            data_bucket bucket;
+            {
+                A b(false, "b", 100);
+
+                bucket.put("abc", std::move(b));
+            }
+
+            BOOST_TEST(constructor_count == 1);
+            BOOST_TEST(move_count == 1);
+            BOOST_TEST(copy_count == 0);
+            BOOST_TEST(destructor_count == 1);
+
+            A *a = bucket.get_ptr<A>("abc");
+            BOOST_TEST(a->content == 100);
+        }
+
+        BOOST_TEST(constructor_count + copy_count + move_count == destructor_count);
+    }
+
+    BOOST_AUTO_TEST_CASE(move_get_then_put_back) {
+        reset_counters();
+
+        {
+            data_bucket bucket;
+            A b(false, "b", 100);
+
+            bucket.put("abc", std::move(b));
+
+            A *a = bucket.get_ptr<A>("abc");
+            BOOST_TEST(a->content == 100);
+        }
+
+        BOOST_TEST(constructor_count + copy_count + move_count == destructor_count);
+        BOOST_TEST(copy_count == 0);
+    }
+
+    BOOST_AUTO_TEST_CASE(copy_get) {
+        reset_counters();
+
+        {
+            data_bucket bucket;
+            A b(false, "b", 100);
+
+            bucket.put("abc", std::move(b));
+
+            A a = bucket.get_copy<A>("abc");
+            BOOST_TEST(a.content == 100);
+        }
+
+        BOOST_TEST(constructor_count + copy_count + move_count == destructor_count);
+    }
+
+    BOOST_AUTO_TEST_CASE(get_primitive_type) {
+        reset_counters();
+
+        {
+            data_bucket bucket;
+            A b(false, "b", 100);
+
+            bucket.put("abc", 1.2f);
+
+            auto a = bucket.get_copy<float>("abc");
+            BOOST_TEST(a == 1.2f);
+        }
+
+        BOOST_TEST(constructor_count + copy_count + move_count == destructor_count);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
