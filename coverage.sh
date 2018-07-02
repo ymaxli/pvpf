12a3d22eae642575936820d90e53238d04f3d2c7
MaxLi1994 <liyue4158@gmail.com>
2018-07-02 15:57:18 -0400

    remove unecessary coverage info

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd build/unit_test/CMakeFiles/pvpf_unit_tests.dir
 lcov --capture --directory . --output-file test.info --test-name test
-genhtml test.info --output-directory output --title "a simple test" --show-details --legend
+lcov --remove test.info '/usr/local/include/*' 'v1' '*/pvpf/unit_test' '*/include/core' -o test_filtered.info
+genhtml test_filtered.info --output-directory output --title "a simple test" --show-details --legend
 cp -r output ../../../coverage
