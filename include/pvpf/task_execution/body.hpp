2d1756e84eebe220aeeaf77fc9a63ce9884f32a5
turtlology <504350677@qq.com>
2018-07-06 18:24:42 -0400

    building context and body from configuration file NOT FINISHED

--- include/pvpf/task_execution/body.hpp
+++ include/pvpf/task_execution/body.hpp
@@ -1,8 +1,37 @@
 //
-// Created by 贺加贝 on 2018/7/5.
+// Created by jiabei on 2018/7/5.
 //
 
 #ifndef POINT_VISION_PIPELINE_FRAMEWORK_BODY_HPP
 #define POINT_VISION_PIPELINE_FRAMEWORK_BODY_HPP
 
 #endif //POINT_VISION_PIPELINE_FRAMEWORK_BODY_HPP
+
+#include <pvpf/pvpf.hpp>
+using namespace std;
+
+PVPF_NAMESPACE_BEGIN
+namespace scheduler{
+    struct context{
+    public:
+        string const node_id;
+        string pre[];
+        string succ[];
+        unordered_map<string, vector< tuple<int, string> >> input;
+        unordered_map<string, string> output;
+        context(string id, string pre[], string succ[],
+                unordered_map<string, vector< tuple<int, string> >> input, unordered_map<string, string> output)
+                : node_id(id), pre(pre), succ(succ), input(input), output(output){}
+    };
+    struct body{
+    public:
+        context context;
+        executable exec;
+        data_io::io_pipe_for_source_node pipe;
+        body(context context, executable exec):context(context), exec(exec){};
+        body(context context, data_io::io_pipe_for_source_node pipe):context(context), pipe(pipe){};
+        void operator();
+    };
+}
+
+PVPF_NAMESPACE_END
---
+++ include/pvpf/task_execution/executable.hpp
@@ -0,0 +1,17 @@
+//
+// Created by jiabei on 2018/7/6.
+//
+
+#ifndef POINT_VISION_PIPELINE_FRAMEWORK_EXECUTABLE_HPP
+#define POINT_VISION_PIPELINE_FRAMEWORK_EXECUTABLE_HPP
+
+#endif //POINT_VISION_PIPELINE_FRAMEWORK_EXECUTABLE_HPP
+
+#include <pvpf/pvpf.hpp>
+#include <pvpf/utils/data_bucket.hpp>
+
+namespace scheduler{
+    class executable{
+        virtual void exec();
+    };
+}
