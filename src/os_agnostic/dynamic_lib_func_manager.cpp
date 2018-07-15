d232d1bf63d65d82da26560ecb1e1c79dc1f4466
MaxLi1994 <liyue4158@gmail.com>
2018-07-15 14:31:28 -0400

    add dynamic_lib_func_manager

---
+++ src/os_agnostic/dynamic_lib_func_manager.cpp
@@ -0,0 +1,239 @@
+//
+// Created by Max on 2018/7/13.
+//
+
+#include <pvpf/pvpf.hpp>
+#include <pvpf/os_agnostic/dynamic_lib_func_manager.hpp>
+#include <dlfcn.h>
+#include <pvpf/utils/exception.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace pvpf;
+
+PVPF_NAMESPACE_BEGIN
+    namespace os_agnostic {
+
+        dylib_func_ptr
+        dynamic_lib_func_manager::load_algorithm(path const &lib_location, std::string const &func_name) {
+            if (algorithm_map.count(lib_location.string())) { // a library been opened before
+                auto &library = algorithm_map[lib_location.string()];
+
+                // find cached function pointers
+                auto &func_ptrs = library.func_ptrs;
+                if (func_ptrs.count(func_name)) { // found
+                    return func_ptrs[func_name];
+                } else { // not found
+#ifdef _WIN32
+
+#else
+                    auto ptr = load_linux_dylib_func_ptr(library.handle, func_name);
+#endif
+
+                    func_ptrs[func_name] = ptr;
+                    return ptr;
+                }
+            } else { // new library
+#ifdef _WIN32
+
+#else
+                void *handle = open_linux_dylib_handle(lib_location.string());
+                auto ptr = load_linux_dylib_func_ptr(handle, func_name);
+#endif
+
+                dynamic_library library;
+                library.handle = handle;
+                library.func_ptrs[func_name] = ptr;
+                algorithm_map[lib_location.string()] = library;
+
+                return ptr;
+            }
+        }
+
+        int dynamic_lib_func_manager::load_input_func(path const &lib_location, std::string const &func_name) {
+            if (io_map.count(lib_location.string())) { // a library been opened before
+                auto &library = io_map[lib_location.string()];
+
+                // find cached function pointers
+                auto &func_ptrs = library.input_func_ptrs;
+                if (func_ptrs.count(func_name)) { // found
+                    return func_ptrs[func_name];
+                } else { // not found
+#ifdef _WIN32
+
+#else
+                    auto ptr = load_linux_input_func_ptr(library.handle, func_name);
+#endif
+
+                    input_func_vector.push_back(ptr);
+                    auto id = static_cast<int>(input_func_vector.size() - 1);
+                    func_ptrs[func_name] = id;
+                    return id;
+                }
+            } else { // new library
+#ifdef _WIN32
+
+#else
+                void *handle = open_linux_dylib_handle(lib_location.string());
+                auto ptr = load_linux_input_func_ptr(handle, func_name);
+#endif
+
+                io_library library;
+                library.handle = handle;
+                input_func_vector.push_back(ptr);
+                auto id = static_cast<int>(input_func_vector.size() - 1);
+                library.input_func_ptrs[func_name] = id;
+                io_map[lib_location.string()] = library;
+                return id;
+            }
+        }
+
+        int dynamic_lib_func_manager::load_output_func(path const &lib_location, std::string const &func_name) {
+            if (io_map.count(lib_location.string())) { // a library been opened before
+                auto &library = io_map[lib_location.string()];
+
+                // find cached function pointers
+                auto &func_ptrs = library.output_func_ptrs;
+                if (func_ptrs.count(func_name)) { // found
+                    return func_ptrs[func_name];
+                } else { // not found
+#ifdef _WIN32
+
+#else
+                    auto ptr = load_linux_output_func_ptr(library.handle, func_name);
+#endif
+                    output_func_vector.push_back(ptr);
+                    auto id = static_cast<int>(output_func_vector.size() - 1);
+                    func_ptrs[func_name] = id;
+                    return id;
+                }
+            } else { // new library
+#ifdef _WIN32
+
+#else
+                void *handle = open_linux_dylib_handle(lib_location.string());
+                auto ptr = load_linux_output_func_ptr(handle, func_name);
+#endif
+
+                io_library library;
+                library.handle = handle;
+                output_func_vector.push_back(ptr);
+                auto id = static_cast<int>(output_func_vector.size() - 1);
+                library.output_func_ptrs[func_name] = id;
+                io_map[lib_location.string()] = library;
+                return id;
+            }
+        }
+
+        dylib_func_ptr dynamic_lib_func_manager::load_linux_dylib_func_ptr(void *handle,
+                                                                           std::string const &func_name) {
+            char *err = nullptr;
+
+            //clear error info
+            dlerror();
+
+            auto pf = (dylib_func_ptr) dlsym(handle, func_name.c_str());
+
+            err = dlerror();
+
+            if (err) {
+                throw utils::pvpf_exception(
+                        (string("could not find function: ") + func_name).c_str());
+            }
+            return pf;
+        }
+
+        input_func_ptr dynamic_lib_func_manager::load_linux_input_func_ptr(void *handle, std::string const &func_name) {
+            char *err = nullptr;
+
+            //clear error info
+            dlerror();
+
+            auto pf = (input_func_ptr) dlsym(handle, func_name.c_str());
+
+            err = dlerror();
+
+            if (err) {
+                throw utils::pvpf_exception(
+                        (string("could not find function: ") + func_name).c_str());
+            }
+            return pf;
+        }
+
+        output_func_ptr
+        dynamic_lib_func_manager::load_linux_output_func_ptr(void *handle, std::string const &func_name) {
+            char *err = nullptr;
+
+            //clear error info
+            dlerror();
+
+            auto pf = (output_func_ptr) dlsym(handle, func_name.c_str());
+
+            err = dlerror();
+
+            if (err) {
+                throw utils::pvpf_exception(
+                        (string("could not find function: ") + func_name).c_str());
+            }
+            return pf;
+        }
+
+        void *dynamic_lib_func_manager::open_linux_dylib_handle(std::string const &lib_location) {
+            void *handle = dlopen(lib_location.c_str(), RTLD_NOW);
+            if (!handle) {
+                throw utils::pvpf_exception((string("could not open library: ") + lib_location).c_str());
+            }
+
+            return handle;
+        }
+
+        void dynamic_lib_func_manager::release_all() {
+            for (auto it: algorithm_map) {
+                dlclose(it.second.handle);
+            }
+
+            for (auto it: io_map) {
+                dlclose(it.second.handle);
+            }
+        }
+
+        dylib_func_ptr
+        dynamic_lib_func_manager::load_windows_dylib_func_ptr(void *handle, std::string const &func_name) {
+            return nullptr;
+        }
+
+        output_func_ptr
+        dynamic_lib_func_manager::load_windows_output_func_ptr(void *handle, std::string const &func_name) {
+            return nullptr;
+        }
+
+        input_func_ptr
+        dynamic_lib_func_manager::load_windows_input_func_ptr(void *handle, std::string const &func_name) {
+            return nullptr;
+        }
+
+        void dynamic_lib_func_manager::invoke_io_func(int io_func_id, data_io::source_io_pipe &pipe) {
+            if (input_func_vector.size() <= io_func_id) {
+                throw utils::pvpf_exception(
+                        (string("input library id: ") + to_string(io_func_id) + string(" not found")).c_str());
+            }
+
+            auto ptr = input_func_vector[io_func_id];
+
+            ptr(pipe);
+        }
+
+        void dynamic_lib_func_manager::invoke_io_func(int io_func_id, data_io::sink_io_pipe &pipe) {
+            if (output_func_vector.size() <= io_func_id) {
+                throw utils::pvpf_exception(
+                        (string("output library id: ") + to_string(io_func_id) + string(" not found")).c_str());
+            }
+
+            auto ptr = output_func_vector[io_func_id];
+
+            ptr(pipe);
+        }
+    }
+PVPF_NAMESPACE_END
+
+
---
+++ unit_test/os_agnostic.dynamic_lib_func_manager.cpp
@@ -0,0 +1,226 @@
+//
+// Created by Max on 2018/7/14.
+//
+#ifdef STAND_ALONE
+#define BOOST_TEST_MODULE OS_AGNOSTIC_DYNAMIC_LIB_FUNC_MANAGER
+#endif
+
+#include <boost/test/unit_test.hpp>
+#include <pvpf/os_agnostic/dynamic_lib_func_manager.hpp>
+#include <boost/filesystem/path.hpp>
+#include <pvpf/utils/data_bucket.hpp>
+#include <pvpf/utils/exception.hpp>
+#include <iostream>
+#include "opencv2/core/core.hpp"
+#include "opencv2/opencv.hpp"
+#include <pvpf/data_io/factory.hpp>
+
+using namespace std;
+using namespace pvpf;
+using namespace pvpf::os_agnostic;
+using namespace boost::filesystem;
+
+string const &TEST_IMAGE_DIR("./test_images/");
+
+dynamic_lib_func_manager &manager = dynamic_lib_func_manager::get_instance();
+
+BOOST_AUTO_TEST_SUITE(os_agnostic_dynamic_lib_func_manager_suite)
+
+    BOOST_AUTO_TEST_CASE(load_algorithm_dylib) {
+        path p("./test_dylib/algorithm.dylib");
+
+        auto ptr = manager.load_algorithm(p, "test1");
+
+        data_bucket data;
+        data_bucket params;
+        auto image = cv::imread(TEST_IMAGE_DIR + string("1.png"));
+        data.put("image", image);
+
+        ptr(data, params);
+
+        bool isEqual = (cv::sum(image != data.get_copy<cv::Mat>("result")) == cv::Scalar(0, 0, 0, 0));
+        BOOST_TEST(isEqual);
+    }
+
+    BOOST_AUTO_TEST_CASE(load_algorithm_dylib_cached) {
+        path p("./test_dylib/algorithm2.dylib");
+
+        auto ptr1 = manager.load_algorithm(p, "test1");
+        auto ptr2 = manager.load_algorithm(p, "test2");
+        auto ptr3 = manager.load_algorithm(p, "test1");
+
+        data_bucket data;
+        data_bucket params;
+        auto image = cv::imread(TEST_IMAGE_DIR + string("1.png"));
+        data.put("image", image);
+
+        ptr1(data, params);
+        bool isEqual = (cv::sum(image != data.get_copy<cv::Mat>("result")) == cv::Scalar(0, 0, 0, 0));
+        BOOST_TEST(isEqual);
+
+        params.put("p", 5);
+        ptr2(data, params);
+        BOOST_TEST(data.get_copy<int>("result") == 5);
+
+        ptr3(data, params);
+        isEqual = (cv::sum(image != data.get_copy<cv::Mat>("result")) == cv::Scalar(0, 0, 0, 0));
+        BOOST_TEST(isEqual);
+    }
+
+    BOOST_AUTO_TEST_CASE(load_algorithm_dylib_exception_1) {
+        path p("./test_dylib/algor.dylib");
+
+        try {
+            manager.load_algorithm(p, "test1");
+
+        } catch (utils::pvpf_exception &e) {
+            BOOST_TEST(string("could not open library: ./test_dylib/algor.dylib") == string(e.what()));
+            return;
+        }
+
+        BOOST_TEST(false);
+    }
+
+    BOOST_AUTO_TEST_CASE(load_algorithm_dylib_exception_2) {
+        path p("./test_dylib/algorithm.dylib");
+
+        try {
+            manager.load_algorithm(p, "tes");
+
+        } catch (utils::pvpf_exception &e) {
+            BOOST_TEST(string("could not find function: tes") == string(e.what()));
+            return;
+        }
+
+        BOOST_TEST(false);
+    }
+
+    BOOST_AUTO_TEST_CASE(load_and_invoke_input_func_dylib) {
+        path p("./test_dylib/io_lib.dylib");
+
+        auto id = manager.load_input_func(p, "input");
+
+        auto pair = data_io::create_source(10, false);
+        auto source_io_pipe = std::move(get<0>(pair));
+        manager.invoke_io_func(id, *source_io_pipe);
+    }
+
+    BOOST_AUTO_TEST_CASE(load_and_invoke_output_func_dylib) {
+        path p("./test_dylib/io_lib.dylib");
+
+        auto id = manager.load_output_func(p, "output");
+
+        auto pair = data_io::create_sink(10, false);
+        auto sink_io_pipe = std::move(get<0>(pair));
+        manager.invoke_io_func(id, *sink_io_pipe);
+    }
+
+    BOOST_AUTO_TEST_CASE(input_func_dylib_cached) {
+        path p("./test_dylib/io_lib.dylib");
+
+        auto id = manager.load_input_func(p, "input");
+
+        BOOST_TEST(id == 0);
+    }
+
+    BOOST_AUTO_TEST_CASE(output_func_dylib_cached) {
+        path p("./test_dylib/io_lib.dylib");
+
+        auto id = manager.load_output_func(p, "output");
+
+        BOOST_TEST(id == 0);
+    }
+
+    BOOST_AUTO_TEST_CASE(load_output_func_dylib_uncached) {
+        path p("./test_dylib/io_lib2.dylib");
+
+        auto id = manager.load_output_func(p, "output");
+
+        BOOST_TEST(id == 1);
+    }
+
+    BOOST_AUTO_TEST_CASE(load_input_func_dylib_exception_1) {
+        path p("./test_dylib/algor.dylib");
+
+        try {
+            manager.load_input_func(p, "test1");
+
+        } catch (utils::pvpf_exception &e) {
+            BOOST_TEST(string("could not open library: ./test_dylib/algor.dylib") == string(e.what()));
+            return;
+        }
+
+        BOOST_TEST(false);
+    }
+
+    BOOST_AUTO_TEST_CASE(load_input_func_dylib_exception_2) {
+        path p("./test_dylib/io_lib.dylib");
+
+        try {
+            manager.load_input_func(p, "test1");
+
+        } catch (utils::pvpf_exception &e) {
+            BOOST_TEST(string("could not find function: test1") == string(e.what()));
+            return;
+        }
+
+        BOOST_TEST(false);
+    }
+
+    BOOST_AUTO_TEST_CASE(load_output_func_dylib_exception_1) {
+        path p("./test_dylib/algor.dylib");
+
+        try {
+            manager.load_output_func(p, "test1");
+
+        } catch (utils::pvpf_exception &e) {
+            BOOST_TEST(string("could not open library: ./test_dylib/algor.dylib") == string(e.what()));
+            return;
+        }
+
+        BOOST_TEST(false);
+    }
+
+    BOOST_AUTO_TEST_CASE(load_output_func_dylib_exception_2) {
+        path p("./test_dylib/io_lib.dylib");
+
+        try {
+            manager.load_output_func(p, "test1");
+
+        } catch (utils::pvpf_exception &e) {
+            BOOST_TEST(string("could not find function: test1") == string(e.what()));
+            return;
+        }
+
+        BOOST_TEST(false);
+    }
+
+    BOOST_AUTO_TEST_CASE(invoke_inputput_func_exception) {
+        try {
+            auto pair = data_io::create_source(10, false);
+            auto source_io_pipe = std::move(get<0>(pair));
+            manager.invoke_io_func(10, *source_io_pipe);
+
+        } catch (utils::pvpf_exception &e) {
+            BOOST_TEST(string("input library id: 10 not found") == string(e.what()));
+            return;
+        }
+
+        BOOST_TEST(false);
+    }
+
+    BOOST_AUTO_TEST_CASE(invoke_output_func_exception) {
+        try {
+            auto pair = data_io::create_sink(10, false);
+            auto sink_io_pipe = std::move(get<0>(pair));
+            manager.invoke_io_func(10, *sink_io_pipe);
+
+        } catch (utils::pvpf_exception &e) {
+            BOOST_TEST(string("output library id: 10 not found") == string(e.what()));
+            return;
+        }
+
+        BOOST_TEST(false);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
