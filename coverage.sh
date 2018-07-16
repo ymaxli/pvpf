5d475edbbb5499b1143edb2c7c0891c8f764eff9
MaxLi1994 <liyue4158@gmail.com>
2018-07-16 11:34:56 -0400

    exclude some files

--- coverage.sh
+++ coverage.sh
@@ -4,7 +4,7 @@ cd build/unit_test/CMakeFiles/pvpf_unit_tests.dir
 rm -r output
 
 lcov --capture --directory . --output-file test.info --test-name test
-lcov --remove test.info '/usr/local/include/*' '/Library/Developer/CommandLineTools/usr/include/c++/*' '*/unit_test/*' '*/include/core/*' -o test_filtered.info
+lcov --remove test.info '/usr/local/include/*' '/Library/Developer/CommandLineTools/usr/include/c++/*' '*/unit_test/*' '*/include/core/*' '/usr/include/*' -o test_filtered.info
 
 genhtml test_filtered.info --output-directory output --title "unit tests code coverage" --show-details --legend
 
