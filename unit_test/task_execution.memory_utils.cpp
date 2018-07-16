e2161d42198f478a8d518a0d530e7c36d6bbdeba
Icay12 <mymaiill@163.com>
2018-07-16 18:08:25 -0400

    add unittest

--- unit_test/task_execution.memory_utils.cpp
+++ unit_test/task_execution.memory_utils.cpp
@@ -24,10 +24,22 @@ BOOST_AUTO_TEST_SUITE(memory_utils_test)
         vector<int> data = {12};
         const shared_ptr<core::any> cpu_ptr = make_shared<core::any>(data);
         shared_ptr<core::any> new_cpu_ptr = copy_cpu(cpu_ptr);
-//        BOOST_CHECK_NE(new_cpu_ptr, cpu_ptr);
-//        BOOST_CHECK_NE(*new_cpu_ptr, *cpu_ptr);
-//        BOOST_CHECK_EQUAL(*new_cpu_ptr, *cpu_ptr);
-//        vector<int> new_data = core::any_cast<vector<int>>(cpu_ptr);
+        vector<int> new_data = core::any_cast<vector<int>>(*cpu_ptr);
+        BOOST_CHECK_EQUAL(new_data.size(), 1);
+        BOOST_CHECK_EQUAL(new_data[0], 12);
+
+    }
+
+    BOOST_AUTO_TEST_CASE(copy_cpu_data_bucket)
+    {
+        std::unordered_map<std::string, std::shared_ptr<core::any>> map;
+        int data1 = 1;
+        map["0"] = make_shared<core::any>(data1);
+//        vector<int> data = {12};
+//        const shared_ptr<core::any> cpu_ptr = make_shared<core::any>(data);
+//        shared_ptr<core::any> new_cpu_ptr = copy_cpu(cpu_ptr);
+//        vector<int> new_data = core::any_cast<vector<int>>(*cpu_ptr);
+//        BOOST_CHECK_EQUAL(new_data.size(), 1);
 //        BOOST_CHECK_EQUAL(new_data[0], 12);
 
     }
